99ad5db82af4107ec94f7d3412a277ffef8299cf
nate <nate@commonshcs.com>
2023-06-22 10:10:15 -0400

    devcontainer config

---
+++ .devcontainer/updateContentCommand.sh
@@ -0,0 +1,6 @@
+npm install
+npm install -g @angular/cli
+mkdir ../commonshcs-angular
+git clone https://github.com/rumor-ml/commonshcs-angular.git ../commonshcs-angular
+cd ../commonshcs-angular
+npm install
